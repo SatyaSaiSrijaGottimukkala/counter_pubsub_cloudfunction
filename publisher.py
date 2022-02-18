008be820dd52970e81c74f596363dd06b2342016
SatyaSaiSrijaGottimukkala <a845604@yara.com>
2022-02-19 00:51:05 +0530

    changes

--- publisher.py
+++ publisher.py
@@ -1,4 +1,5 @@
 import os
+import json
 from google.cloud import pubsub_v1
 PROJECT_ID = os.getenv("cedar-chemist-341514")
 credentials_path = "/Users/a845604yara.com/Documents/gcloud-practice/pubsub-cloudfunction/cedar-chemist-341514-1b7b579e88f1.json"
@@ -6,8 +7,11 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS']= credentials_path
 publisher = pubsub_v1.PublisherClient()
 topic_path = 'projects/cedar-chemist-341514/topics/counter-responses'
 
-data = input("enter starting value : ")
-data = data.encode('utf-8')
+data = int(input("enter starting value : "))
 
-future = publisher.publish(topic_path, data)
+data_json = json.dumps({"counter":data}).encode('utf-8')
+
+future = publisher.publish(topic_path, data= data_json)
 print(f'publish image id {future.result()}')
+
+
